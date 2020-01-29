2e853850b1a21e98db4c338287525e78faad7d9b
Lukas Diekmann <l.diekmann@tricks.de>
2020-01-29 08:59:53 +0100

    (fix) zeit.de

--- content-script.js
+++ content-script.js
@@ -1,7 +1,7 @@
 let filters = [
     {
         "url": "^https?://www.zeit.de",
-        "elements": ["#comments", ".metadata__commentcount", ".zon-teaser-standard__commentcount"]
+        "elements": ["#comments", ".metadata__commentcount", ".zon-teaser-standard__commentcount", ".zon-teaser-lead__commentcount"]
     },
     {
         "url": "^https?://www.nw.de",
